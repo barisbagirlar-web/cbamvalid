72325c861fda5bd1339e3c4ec6d3186c9a9fa617
barisbagirlar-web <barisbagirlar@gmail.com>
2026-07-16 02:16:00 +0300

    fix(commerce): validate Paddle before checkout creation

--- functions/src/commerce/paddle/checkout-service.ts
+++ functions/src/commerce/paddle/checkout-service.ts
@@ -1,16 +1,16 @@
 import { getPriceIdForProduct, getProduct } from "../catalog";
-import { paddle, isSandboxMode } from "../paddle-client";
+import { assertPaddleConfigured, paddle, isSandboxMode } from "../paddle-client";
 import { adminDb } from "../../firebase-admin";
 import { createOrder, transitionOrderStatus } from "../order-service";
 import { verifyCaseOwner } from "../../cbam/storage/case-repository";
 
 export async function createCheckout(params: {
   uid: string;
-  email: string;
   productCode: string;
   requestId: string;
   caseId?: string;
 }): Promise<string> {
+  assertPaddleConfigured();
   const product = getProduct(params.productCode);
   if (!product) throw new Error("CHECKOUT_PRODUCT_INVALID");
   if (params.caseId) await verifyCaseOwner(params.caseId, params.uid);
@@ -34,7 +34,6 @@ export async function createCheckout(params: {
   try {
     const paddleTransaction = await paddle.transactions.create({
       items: [{ priceId, quantity: 1 }],
-      customer: params.email ? { email: params.email } : undefined,
       customData: {
         uid: params.uid,
         orderId: result.order.orderId,
