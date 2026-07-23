01680453d90f96ff203705e792187dcf21c50dcb
Baris Bagirlar <barisbagirlar@gmail.com>
2026-07-23 21:53:49 +0300

    feat: link raw stylesheet in public layout and add static sitemap and robots

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,6 +1,6 @@
 import { PublicHeader } from "@/components/layout/PublicHeader";
 import AppFooter from "@/components/layout/AppFooter";
-import "./style.css";
+import Script from "next/script";
 
 export default function PublicLayout({
   children,
@@ -8,10 +8,18 @@ export default function PublicLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="cbam-public">
+    <>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Fraunces:ital,wght@0,500;0,560;0,600;1,500;1,560&family=Inter:wght@400;500;600&family=IBM+Plex+Mono:wght@400;500;600&display=swap"
+        rel="stylesheet"
+      />
+      <link rel="stylesheet" href="/assets/css/style.css" />
       <PublicHeader />
       {children}
       <AppFooter />
-    </div>
+      <Script src="/assets/js/main.js" strategy="afterInteractive" />
+    </>
   );
 }
